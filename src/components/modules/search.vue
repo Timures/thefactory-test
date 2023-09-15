85e8c3a45189af11263a0a89b45ecba1745ced71
Timures <bisemsk@gmail.com>
2023-09-15 22:48:25 +0600

    add search component

--- src/components/modules/search.vue
+++ src/components/modules/search.vue
@@ -1,5 +1,60 @@
 <template>
-    <div>
-        search
+    <div class="search">
+      <input v-model="searchTerm" @input="search" placeholder="Поиск..." />
+      <ul>
+        <li v-for="result in searchResults" :key="result.id">
+          {{ result.name }}
+        </li>
+      </ul>
     </div>
-</template>
+  </template>
+  
+  <script>
+  import { ref, watch } from "vue";
+  
+  export default {
+    setup() {
+      const searchTerm = ref("");
+      const searchResults = ref([]);
+  
+      const search = () => {
+        // Здесь ты можешь добавить логику поиска, например, отправку запроса на сервер или фильтрацию данных.
+        // В этом примере пусто, но ты можешь настроить его в соответствии с твоими потребностями.
+      };
+  
+      // Следим за изменениями searchTerm и вызываем функцию поиска
+      watch(searchTerm, () => {
+        search();
+      });
+  
+      return {
+        searchTerm,
+        searchResults,
+        search,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .search {
+    margin: 20px;
+  }
+  
+  input {
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    margin: 5px 0;
+  }
+  </style>
+  
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 import { ref, onMounted } from 'vue'; // Импортируем ref и onMounted
 
 const ApiKey = 'mlwN4BpfAUNKQdXZDOxu0f0R_4TC8iV6dzj8yIC6zA0';
@@ -6,11 +7,12 @@ const baseURL = 'https://api.unsplash.com/';
 
 // Реактивная переменная для хранения загруженных фотографий
 const photos = ref([]);
+const searchTerm = ref('');
 
 // Метод для загрузки фотографий с использованием fetch
 const fetchPhotos = async () => {
   try {
-    const response = await fetch(`${baseURL}/photos?client_id=${ApiKey}&orientation=squarish&per_page=9`); // Замените URL на ваш
+    const response = await fetch(`${baseURL}/photos?client_id=${ApiKey}&orientation=squarish&per_page=9`);
     if (!response.ok) {
       throw new Error('Ошибка загрузки фотографий');
     }
@@ -22,6 +24,22 @@ const fetchPhotos = async () => {
   }
 };
 
+const performSearch = async () => {
+  try {
+    const response = await fetch(`${baseURL}search/photos?client_id=${ApiKey}&query=${searchTerm.value}&orientation=squarish&per_page=9`);
+    console.log('Выполняем поиск с запросом:', searchTerm.value);
+    if (!response.ok) {
+      throw new Error('Ошибка загрузки фотографий');
+    }
+    const data = await response.json();
+    photos.value = data.results; // Присваиваем полученные данные переменной photos
+    // console.log(photos.value.id)
+  } catch (error) {
+    console.error('Ошибка при загрузке фотографий:', error);
+  }
+
+};
+
 // Вызываем метод fetchPhotos при открытии страницы
 onMounted(() => {
   fetchPhotos();
@@ -29,44 +47,124 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="container">
-    <div class="photos-list">
-      <router-link class="photo" v-for="(photo, index) in photos" :key="photo.id" :to="{ name: 'PhotoDetail', params: { id: photo.id } }">
-        <img :src="photo.urls.small" :alt="photo.alt_description">
-      </router-link>
+  <main>
+    <div class="photos-search">
+      <div class="container">
+        <div class="search-input">
+          <input v-model="searchTerm" placeholder="Поиск..." type="text" />
+        <button @click="performSearch">
+        <img src="@/assets/icons/search.svg" alt="search icon"></button>
+        </div>
+      </div>
     </div>
-  </div>
+
+    <div class="container">
+      <div class="photos-list">
+        <router-link class="photo" v-for="photo in photos" :key="photo.id"
+          :to="{ name: 'PhotoDetail', params: { id: photo.id } }">
+          <img :src="photo.urls.small" :alt="photo.alt_description">
+        </router-link>
+      </div>
+    </div>
+  </main>
 </template>
 
 
 <style lang="scss" scoped>
-.photos-list {
-  padding: 45px 20px 43px;
-  display: grid;
-  grid-template-columns: auto;
-  grid-template-rows: auto;
-  gap: 16px;
-
-  @media (min-width: 576px) {
-    padding: 107px 0 71px;
-    gap: 30px;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: auto auto auto;
+.photos-search {
+  background-image: url('@/assets/img/vgog.jpeg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  padding: 80px 22px;
+    // Large devices (desktops, 992px and up)
+@media (min-width: 992px) { 
+  padding: 90px 22px;
+   
   }
 }
+.search-input {
+  position: relative;
+  display: flex;
+  max-width: 866px;
+  margin: 0 auto;
 
-.photo {
-  img {
+  input[type="text"] {
+    height: 70px;
     width: 100%;
-    height: 312px;
-    object-fit: cover;
-    border-radius: 8px;
-  }
+   
+    background-color: #fff;
+    padding: 0 25px;
+    border: none;
+    outline: none;
+    display: flex;
+    align-items: center;
 
-  @media (min-width: 576px) {
+    color: #000;
+    font-family: Roboto;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    background-color: transparent;
+    border: none;
+    box-shadow: none;
+    padding: 0;
+    position: absolute;
+    right: 25px;
+    top: calc(50% - 12px);
+    width: 24px;
     img {
-      height: 440px;
+        width: 100%;
     }
   }
 }
+
+  .photos-list {
+    padding: 45px 20px 43px;
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: auto;
+    gap: 16px;
+
+    // Small devices (landscape phones, 576px and up)
+    @media (min-width: 576px) {
+      // padding: 107px 0 71px;
+      
+      
+      grid-template-rows: auto ;
+    }
+
+    // Medium devices (tablets, 768px and up)
+@media (min-width: 768px) { 
+  grid-template-columns: auto auto ;
+    grid-template-rows: auto;
+ }
+    // Large devices (desktops, 992px and up)
+@media (min-width: 992px) { 
+  gap: 30px;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 107px 20px 71px; }
+   
+  }
+
+  .photo {
+    img {
+      width: 100%;
+      height: 312px;
+      object-fit: cover;
+      border-radius: 8px;
+    }
+
+    @media (min-width: 576px) {
+      img {
+        height: 440px;
+      }
+    }
+  }
 </style>
